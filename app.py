8e67d66f708b841dfad32b95d55b6e2ad183158d
neuerstandard <vertrieb@neuer-standard.de>
2025-05-10 15:31:56 +0200

    Fix Farbangabe für Hintergrund: korrektes RGB gesetzt

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ def download():
     c.setAuthor("neuerstandard")
     c.setSubject("A recursively generated poster that encodes a link to its own identical copy.")
 
-    c.setFillColorRGB(170 / 158, 144)
+    c.setFillColorRGB(170 / 255, 158 / 255, 144 / 255)
+
     c.rect(0, 0, w_pt, h_pt, fill=1, stroke=0)
 
     c.setFillColorRGB(0, 0, 0)
